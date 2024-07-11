b39be88b2233832b63381356383460c91902a1e5
Dmytro Lukianov <dimalukianovweb@gmail.com>
2024-07-11 14:09:11 +0300

    add wait status for button

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -3,12 +3,12 @@
 import { FC, useRef } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 import { addTodo } from "@/app/_actions/add-todo";
 
 export const AddTodoForm = (): React.ReactNode => {
   const formRef = useRef<HTMLFormElement>(null);
-  const [errors, action, isPending] = useFormState(addTodo, {});
+  const [errors, action] = useFormState(addTodo, {});
 
   if (!errors) {
     formRef?.current?.reset();
@@ -22,9 +22,7 @@ export const AddTodoForm = (): React.ReactNode => {
         className="flex w-full max-w-sm items-center space-x-2"
       >
         <Input type="new task" placeholder="new task" name="todo" />
-        <Button type="submit" disabled={isPending}>
-          Add
-        </Button>
+        <SubmitButton />
       </form>
       {errors?.todo && (
         <span className="text-destructive text-sm">{errors?.todo}</span>
@@ -32,3 +30,13 @@ export const AddTodoForm = (): React.ReactNode => {
     </>
   );
 };
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Adding..." : "Аdd"}
+    </Button>
+  );
+}
